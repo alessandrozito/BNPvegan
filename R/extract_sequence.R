9313a051aa4a0e88913e9446dfc4e65d99fd7e09
Tommaso Rigon <tommaso.rigon@gmail.com>
2020-11-30 14:26:51 +0100

    Graphical improvement

--- R/extract_sequence.R
+++ R/extract_sequence.R
@@ -6,32 +6,33 @@
 #' @return vector
 #' @export
 #'
-#' @examples build_random_sequences(c(1,2,45,7))
-extract_sequence = function(species_counts, seed = 42){
+#' @examples
+#' build_random_sequences(c(1, 2, 45, 7))
+extract_sequence <- function(species_counts, seed = 42) {
 
   # Turn the count function into an integer list
-  species_counts = round(species_counts)
+  species_counts <- round(species_counts)
 
   # Extract the names of the species in the sequence
-  if(is.null(names(names(species_counts)))){
-    names(species_counts) = paste0("sp",as.character(c(1:length(species_counts))))
+  if (is.null(names(names(species_counts)))) {
+    names(species_counts) <- paste0("sp", as.character(c(1:length(species_counts))))
   }
-  species_names = names(species_counts)
+  species_names <- names(species_counts)
 
   # Filter out the observations with counts equal to 0
-  species_names = species_names[species_counts > 0]
-  species_counts = species_counts[species_counts > 0]
+  species_names <- species_names[species_counts > 0]
+  species_counts <- species_counts[species_counts > 0]
 
   # Extract the crude sequence
-  sequence = c()
-  for(j in 1:length(species_counts)){
-    subseq = rep(x = species_names[j], times = as.numeric(species_counts[j]))
-    sequence = c(sequence, subseq)
+  sequence <- c()
+  for (j in 1:length(species_counts)) {
+    subseq <- rep(x = species_names[j], times = as.numeric(species_counts[j]))
+    sequence <- c(sequence, subseq)
   }
 
   # Randomize it
   set.seed(seed)
-  sequence = sample(sequence, size = length(sequence), replace = FALSE)
+  sequence <- sample(sequence, size = length(sequence), replace = FALSE)
 
   return(sequence)
 }
