d8eae5c9fa08adf09756da8f45ab347a65e810b3
Tommaso Rigon <tommaso.rigon@gmail.com>
2020-11-30 14:45:44 +0100

    Graphical improvements

--- R/extract_discoveries.R
+++ R/extract_discoveries.R
@@ -5,16 +5,16 @@
 #' @return A sequence of binary variables
 #' @export
 #'
-#' @examples extract_discoveries(c("sp1", "sp1", "sp3", "sp4"))
-extract_discoveries = function(sequence){
-
-  species_appeared = NULL
-  m = length(sequence)
-  discoveries = rep(0, m)
-  for(i in 1:m){
-    if(!(sequence[i] %in% species_appeared)){
-      discoveries[i] = 1
-      species_appeared = c(species_appeared, sequence[i])
+#' @examples
+#' extract_discoveries(c("sp1", "sp1", "sp3", "sp4"))
+extract_discoveries <- function(sequence) {
+  species_appeared <- NULL
+  m <- length(sequence)
+  discoveries <- rep(0, m)
+  for (i in 1:m) {
+    if (!(sequence[i] %in% species_appeared)) {
+      discoveries[i] <- 1
+      species_appeared <- c(species_appeared, sequence[i])
     }
   }
   return(discoveries)
--- R/fit_LL3.R
+++ R/fit_LL3.R
@@ -5,23 +5,24 @@
 #' @return A list, including the parameters
 #' @export
 #'
-#' @examples fit_LL3(c(1,0,1, 1,0,1, 0, 0,0))
-fit_LL3 = function(discoveries){
+#' @examples
+#' fit_LL3(c(1, 0, 1, 1, 0, 1, 0, 0, 0))
+fit_LL3 <- function(discoveries) {
 
   # Step 1 - introduce useful quantities
-  N = length(discoveries)
-  X = as.matrix(cbind(1, log(1:(N-1)), c(1:(N-1))))  # Predictors are 1,log(n),n
+  N <- length(discoveries)
+  X <- as.matrix(cbind(1, log(1:(N - 1)), c(1:(N - 1)))) # Predictors are 1,log(n),n
 
   # Step 2 - Fit the logistic regression via fastglm
-  glmfit = fastglm::fastglmPure(y = discoveries[-1], x = X, family = binomial("logit"), method = 3)
-  pars = c("alpha" = exp(glmfit$coefficients[1]), "sigma" = 1 + glmfit$coefficients[2], "phi" =  exp(glmfit$coefficients[3]))
+  glmfit <- fastglm::fastglmPure(y = discoveries[-1], x = X, family = binomial("logit"), method = 3)
+  pars <- c("alpha" = exp(glmfit$coefficients[1]), "sigma" = 1 + glmfit$coefficients[2], "phi" = exp(glmfit$coefficients[3]))
 
   # Step 4 - Check if the parameters are in bound
-  if(pars[3] > 1 | pars[2] >= 1){
+  if (pars[3] > 1 | pars[2] >= 1) {
     # Need to re-estimate the model using the two parameter log-logistic.
-    out = fit_LL2(discoveries)
-    out$parameters = c(out$parameters, "phi" = 1)
-    out$Out_of_bound = TRUE
+    out <- fit_LL2(discoveries)
+    out$parameters <- c(out$parameters, "phi" = 1)
+    out$Out_of_bound <- TRUE
 
     return(out)
   } else {
