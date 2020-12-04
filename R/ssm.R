708189759f0f5d657acbd389477748996ad4dd04
Tommaso Rigon <tommaso.rigon@gmail.com>
2020-12-04 19:38:35 +0100

    Major updates

--- R/ssm.R
+++ R/ssm.R
@@ -4,24 +4,61 @@
 #' @param frequencies A \code{K}-dimensional vector of frequencies
 #' @param model Model to fit. Available models are "DP" (Dirichlet Process) and "PY" (Pitman-Yor process)
 #' #'
-#' @return a list (?)
+#' @return An object of class "ssm"
+#'
 #' @export
 ssm <- function(frequencies, model){
 
   if (model == "DP") {
     fit <- max_EPPF_DP(frequencies)
-    out <- list(freq = freq, param = fit$par, loglik = - fit$objective)
+    out <- list(frequencies = frequencies, param = fit$par, loglik = - fit$objective)
     class(out) <- c("ssm","DP")
     return(out)
   }
 
   if (model == "PY") {
     fit <- max_EPPF_PY(frequencies)
-    out <- list(freq = freq, param = fit$par, loglik = - fit$objective)
+    out <- list(frequencies = frequencies, param = fit$par, loglik = - fit$objective)
     class(out) <- c("ssm","PY")
     return(out)
   }
 }
 
+
+#' Predict method for the DP model
+#'
+#'
+#' Predict method for a DP model, producing either the rarefaction curve or the extrapolation curve.
+#'
+#' @param object An object of class \code{\link[ssm]{ssm}}.
+#' @param newdata A new data frame containing
+#' @param ... Further arguments passed to or from other methods.
+#'
+#' @details The method...
+#'
+#' @export
+#'
+predict.DP <- function(object, newdata=NULL, ...) {
+  n <- length(object$frequencies)
+  expected_cl_py(1:n, sigma = 0, alpha = object$param)
+}
+
+#' Predict method for the DP model
+#'
+#'
+#' Predict method for a DP model, producing either the rarefaction curve or the extrapolation curve.
+#'
+#' @param object An object of class \code{\link[ssm]{ssm}}.
+#' @param newdata A new data frame containing
+#' @param ... Further arguments passed to or from other methods.
+#'
+#' @details The method...
+#'
+#' @export
+#'
+predict.PY <- function(object, newdata=NULL, ...) {
+  n <- length(object$frequencies)
+  expected_cl_py(1:n, sigma = object$param[2], alpha = object$param[1])
+}
 
 
--- R/ssm_internal.R
+++ R/ssm_internal.R
@@ -1,22 +1,24 @@
-expected_cl_py <- function(n, alpha, theta) {
+expected_cl_py <- function(n, sigma, alpha) {
   n <- as.integer(n)
-  if (alpha == 0) {
-    out <- theta * sum(1 / (theta - 1 + 1:n))
+  if (sigma == 0) {
+    out <- alpha * sum(1 / (alpha - 1 + 1:n))
   } else {
-    out <- 1 / alpha * exp(lgamma(theta + alpha + n) - lgamma(theta + alpha) - lgamma(theta + n) + lgamma(theta + 1)) - theta / alpha
+    out <- 1 / sigma * exp(lgamma(alpha + sigma + n) - lgamma(alpha + sigma) - lgamma(alpha + n) + lgamma(alpha + 1)) - alpha / sigma
   }
 
   return(out)
 }
+expected_cl_py <- Vectorize(expected_cl_py, vectorize.args = "n")
 
-expected_m_dp <- function(m, n, theta) {
-  out <- log(theta) + lchoose(n, m) + lgamma(m) + lgamma(theta + n - m) - lgamma(theta + n)
+
+expected_m_dp <- function(m, n, alpha) {
+  out <- log(alpha) + lchoose(n, m) + lgamma(m) + lgamma(alpha + n - m) - lgamma(alpha + n)
   exp(out)
 }
 expected_m_dp <- Vectorize(expected_m_dp, vectorize.args = "m")
 
-expected_m_py <- function(m, n, alpha, theta) {
-  out <- log(theta) + lchoose(n, m) + lgamma(m - alpha) - lgamma(1 - alpha) - lgamma(theta + n) + lgamma(theta) + lgamma(theta + alpha + n - m) - lgamma(theta + alpha)
+expected_m_py <- function(m, n, sigma, alpha) {
+  out <- log(alpha) + lchoose(n, m) + lgamma(m - sigma) - lgamma(1 - sigma) - lgamma(alpha + n) + lgamma(alpha) + lgamma(alpha + sigma + n - m) - lgamma(alpha + sigma)
   exp(out)
 }
 expected_m_py <- Vectorize(expected_m_py, vectorize.args = "m")
@@ -24,8 +26,8 @@ expected_m_py <- Vectorize(expected_m_py, vectorize.args = "m")
 frequency_check_PY <- function(frequencies) {
   n <- sum(frequencies)
   fit_PY <- max_EPPF_PY(frequencies)
-  theta <- fit_PY$par[1]
-  alpha <- fit_PY$par[2]
+  alpha <- fit_PY$par[1]
+  sigma <- fit_PY$par[2]
 
 
   M_l <- as.numeric(table(factor(frequencies, levels = 1:n)))
@@ -33,7 +35,7 @@ frequency_check_PY <- function(frequencies) {
 
   idx <- 1:(which.min(M_l) - 1) # which(P_l > 0)
 
-  data_plot <- data.frame(Size = idx, M_l = M_l[idx], Theoretical = expected_m_py(idx, n = n, alpha = alpha, theta = theta))
+  data_plot <- data.frame(Size = idx, M_l = M_l[idx], Theoretical = expected_m_py(idx, n = n, sigma = sigma, alpha = alpha))
   p <- ggplot(data = data_plot, aes(x = Size, y = M_l)) +
     geom_point() +
     geom_line(aes(y = Theoretical), color = "blue", linetype = "dashed") +
