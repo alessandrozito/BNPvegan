1bd49e5586e3b021f8d1f8d97834e53d6a9dc57d
Tommaso Rigon <tommaso.rigon@gmail.com>
2020-12-04 19:48:19 +0100

    asd

--- R/sdm_miscellanea.R
+++ R/sdm_miscellanea.R
@@ -24,39 +24,23 @@ extract_discoveries <- function(sequence) {
 
 #' Build a random sequence of observed species form a vector of counts
 #'
-#' @param species_counts Vector of counts of the species observed
-#' @param seed Optional seed. Default is 42
+#' @param frequencies Vector of counts of the species observed
 #'
 #' @return vector
 #' @export
 #'
 #' @examples
-#' build_random_sequences(c(1, 2, 45, 7))
-extract_sequence <- function(species_counts, seed = 42) {
+#' sample_sequence(c(1, 2, 45, 7))
+sample_sequence <- function(frequencies) {
 
-  # Turn the count function into an integer list
-  species_counts <- round(species_counts)
-
-  # Extract the names of the species in the sequence
-  if (is.null(names(names(species_counts)))) {
-    names(species_counts) <- paste0("sp", as.character(c(1:length(species_counts))))
-  }
-  species_names <- names(species_counts)
-
-  # Filter out the observations with counts equal to 0
-  species_names <- species_names[species_counts > 0]
-  species_counts <- species_counts[species_counts > 0]
+  n <- sum(frequencies)
+  K <- length(frequencies)
 
   # Extract the crude sequence
-  sequence <- c()
-  for (j in 1:length(species_counts)) {
-    subseq <- rep(x = species_names[j], times = as.numeric(species_counts[j]))
-    sequence <- c(sequence, subseq)
-  }
+  sequence <- rep(1:K, each=frequencies)
 
   # Randomize it
-  set.seed(seed)
-  sequence <- sample(sequence, size = length(sequence), replace = FALSE)
+  sequence <- sample(sequence, size = n, replace = FALSE)
 
   return(sequence)
 }
