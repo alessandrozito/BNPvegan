c6dd422bde3adf1efc88fed15f3c131126775a0d
Tommaso Rigon <tommaso.rigon@gmail.com>
2021-02-04 17:09:53 +0100

    Re-organization

--- R/ssm_internal.R
+++ R/ssm_internal.R
@@ -1,42 +1,54 @@
-expected_cl_py <- function(n, sigma, alpha) {
-  n <- as.integer(n)
-  if (sigma == 0) {
-    out <- alpha * sum(1 / (alpha - 1 + 1:n))
-  } else {
-    out <- 1 / sigma * exp(lgamma(alpha + sigma + n) - lgamma(alpha + sigma) - lgamma(alpha + n) + lgamma(alpha + 1)) - alpha / sigma
+#' @import stats
+#' @importFrom Rcpp evalCpp sourceCpp
+#' @importFrom vegan diversity
+#'
+
+
+
+logEPPF_PY <- function(alpha, sigma, frequencies) {
+  if (any(sigma < 0, alpha <= -sigma + 1e-04)) {
+    return(-Inf)
   }
 
+  # Sample size
+  n <- sum(frequencies)
+  # Number of frequencies
+  K <- length(frequencies)
+  # Loglikelihood
+  loglik <- sum(log(alpha + 1:(K - 1) * sigma)) - lgamma(alpha + n) + lgamma(alpha + 1) + sum(lgamma(frequencies - sigma)) - K * lgamma(1 - sigma)
+
+  loglik
+}
+
+logEPPF_DP <- function(alpha, frequencies) {
+
+  # Sample size
+  n <- sum(frequencies)
+  # Number of frequencies
+  K <- length(frequencies)
+
+  # Loglikelihood
+  loglik <- K * log(alpha) - lgamma(alpha + n) + lgamma(alpha)
+
+  loglik
+}
+
+max_EPPF_PY <- function(frequencies) {
+  start <- c(1, 0.5) # Initialization of the maximization algorithm
+  out <- nlminb(
+    start = start,
+    function(param) -logEPPF_PY(alpha = param[1], sigma = param[2], frequencies = frequencies),
+    lower = c(-Inf, 1e-16), upper = c(Inf, 1 - 1e-10)
+  )
   return(out)
 }
-#' @export
-expected_cl_py <- Vectorize(expected_cl_py, vectorize.args = "n")
 
-
-expected_m_dp <- function(m, n, alpha) {
-  out <- log(alpha) + lchoose(n, m) + lgamma(m) + lgamma(alpha + n - m) - lgamma(alpha + n)
-  exp(out)
-}
-expected_m_dp <- Vectorize(expected_m_dp, vectorize.args = "m")
-
-expected_m_py <- function(m, n, sigma, alpha) {
-  out <- log(alpha) + lchoose(n, m) + lgamma(m - sigma) - lgamma(1 - sigma) - lgamma(alpha + n) + lgamma(alpha) + lgamma(alpha + sigma + n - m) - lgamma(alpha + sigma)
-  exp(out)
-}
-
-#' @export
-expected_m_py <- Vectorize(expected_m_py, vectorize.args = "m")
-
-
-extrapolate_cl_py <- function(m, K, n, sigma, alpha) {
-  n <- as.integer(n)
-  # if (sigma == 0) {
-  #  out <- alpha * sum(1 / (alpha - 1 + 1:n))
-  # } else {
-  out <- (K + alpha / sigma) * (exp(lgamma(alpha + n + sigma + m) - lgamma(alpha + n + sigma) - lgamma(alpha + n + m) + lgamma(alpha + n)) - 1)
-  # }
-
+max_EPPF_DP <- function(frequencies) {
+  start <- 1 # Initialization of the maximization algorithm
+  out <- nlminb(
+    start = start,
+    function(param) -logEPPF_DP(alpha = param, frequencies = frequencies),
+    lower = 1e-10, upper = Inf
+  )
   return(out)
 }
-
-#' @export
-extrapolate_cl_py <- Vectorize(extrapolate_cl_py, vectorize.args = "m")
