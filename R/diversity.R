9747ae2ae4e15b239a4535837fe886e86a8fcd1c
Tommaso Rigon <tommaso.rigon@gmail.com>
2021-02-04 17:08:08 +0100

    Re-organization

--- R/diversity.R
+++ R/diversity.R
@@ -1,3 +1,34 @@
+Simpson <- function(frequencies) {
+  freq_rel <- frequencies / sum(frequencies)
+  out <- sum(freq_rel^2)
+}
+
+Shannon <- function(frequencies) {
+  freq_rel <- frequencies / sum(frequencies)
+  -sum(freq_rel * log(freq_rel))
+}
+
+Gini <- function(frequencies) {
+  1 - Simpson(frequencies)
+}
+
+Gini_norm <- function(frequencies) {
+  K <- length(frequencies)
+  G <- Gini(frequencies)
+  G * K / (K - 1)
+}
+
+Shannon_norm <- function(frequencies) {
+  K <- length(frequencies)
+  H <- Shannon(frequencies)
+  H / log(K)
+}
+
+unique_ratio <- function(frequencies) {
+  mean(frequencies == 1)
+}
+
+
 #' Diversity measures
 #'
 #'
