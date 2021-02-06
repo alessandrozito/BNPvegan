5ec4adfbf5bda8ce8d3d09bfed51d0140de3bf2b
Tommaso Rigon <tommaso.rigon@gmail.com>
2021-02-06 22:04:58 +0100

    Updates

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -20,12 +20,12 @@ BEGIN_RCPP
 END_RCPP
 }
 // rarefy_C
-vec rarefy_C(vec freq, int n, int K);
+arma::vec rarefy_C(arma::vec freq, int n, int K);
 RcppExport SEXP _BNPvegan_rarefy_C(SEXP freqSEXP, SEXP nSEXP, SEXP KSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< vec >::type freq(freqSEXP);
+    Rcpp::traits::input_parameter< arma::vec >::type freq(freqSEXP);
     Rcpp::traits::input_parameter< int >::type n(nSEXP);
     Rcpp::traits::input_parameter< int >::type K(KSEXP);
     rcpp_result_gen = Rcpp::wrap(rarefy_C(freq, n, K));
